6579057945258940a84432fd3fda490c2d66e755
Daniel Arroyo <daniel.arroyo.cortes@avanade.com>
2023-06-06 10:28:13 +0200

    ExcepcionesGlobales

---
+++ Excepciones/Middlewares/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Text.Json;
+
+namespace Excepciones.Middlewares
+{
+    public class GlobalExceptionHandlingMiddleware:IMiddleware
+    {
+        // todo middleware para gestionar errores
+        private readonly ILogger<GlobalExceptionHandlingMiddleware> _logger;
+        public GlobalExceptionHandlingMiddleware(
+            ILogger<GlobalExceptionHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(
+            HttpContext context, 
+            RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                // Guardamos error, todo, logger servicio externo, como se haría???
+                _logger.LogError(ex, ex.Message);
+
+                // Modificamos response para devovler
+                context.Response.StatusCode = 
+                    (int)HttpStatusCode.InternalServerError;
+
+                // Customizamos respuesta
+                ProblemDetails problem = new ProblemDetails()
+                {
+                    Status = (int)HttpStatusCode.InternalServerError,
+                    Type = "Server Error",
+                    Title = "Server Error Title",
+                    Detail = "Internal server error has ocurred"
+                };
+                
+                var json = JsonSerializer.Serialize(problem);
+                context.Response.ContentType = "application/json";
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
---
+++ Excepciones/Program.cs
@@ -0,0 +1,32 @@
+using Excepciones.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// todo GlobalExceptionHandlingMiddleware
+builder.Services.AddTransient<GlobalExceptionHandlingMiddleware>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// todo GlobalExceptionHandlingMiddleware
+app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
+
+app.MapControllers();
+
+app.Run();
